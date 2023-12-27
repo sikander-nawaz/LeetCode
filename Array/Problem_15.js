aa7cdb3fc1bfcdd25e028a0d5560c6c17eba93e3
JadoGarBhai <sikandernawaz35@gmail.com>
2023-12-27 23:32:03 +0500

    15

---
+++ Array/Problem_15.js
@@ -0,0 +1,28 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// SOLUTION:
+
+var twoSum = function (nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        result.push(i, j);
+      }
+    }
+  }
+
+  return result;
+};
+
+let nums = [3, 2, 4];
+let target = 6;
+console.log(twoSum(nums, target));
