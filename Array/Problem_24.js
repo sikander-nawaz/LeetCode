2d26287ec70302ccdb6b02917c4972540c4e5b74
JadoGarBhai <sikandernawaz35@gmail.com>
2024-01-05 22:54:00 +0500

    23

---
+++ Array/Problem_24.js
@@ -0,0 +1,14 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+// Example 1:
+// Input: s = "Hello"
+// Output: "hello"
+
+// SOLUTION:
+
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+let s = "Hello";
+console.log(toLowerCase(s));
