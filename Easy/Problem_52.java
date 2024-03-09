1d84e479f847bb7745c2c99073309c6a3c17e355
JadoGarBhai <sikandernawaz35@gmail.com>
2024-03-09 22:34:43 +0500

    52

---
+++ Easy/Problem_52.java
@@ -0,0 +1,30 @@
+// Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+// Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+// Example 1:
+// Input: nums1 = [1,2,3], nums2 = [2,4]
+// Output: 2
+// Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+
+// SOLUTION:
+
+
+public class Problem_52 {
+     public int getCommon(int[] nums1, int[] nums2) {
+          int pointer1 = 0;
+          int pointer2 = 0;
+  
+          while (pointer1 < nums1.length && pointer2 < nums2.length) {
+              if (nums1[pointer1] == nums2[pointer2]) {
+                  return nums1[pointer1];
+              } else if (nums1[pointer1] < nums2[pointer2]) {
+                  pointer1++; 
+              } else {
+                  pointer2++; 
+              }
+          }
+          
+          return -1;
+      }
+}
--- README.md
+++ README.md
@@ -53,6 +53,7 @@
 49. [Arranging Coins](https://leetcode.com/problems/arranging-coins)
 50. [Middle of the Linked List](https://leetcode.com/problems/middle-of-the-linked-list)
 51. [Count Elements With Maximum Frequency](https://leetcode.com/problems/count-elements-with-maximum-frequency)
+52. [Minimum Common Value](https://leetcode.com/problems/minimum-common-value)
 
 - ## Medium
 
