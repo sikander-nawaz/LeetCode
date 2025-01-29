5d0060dd76782573b589f1c6c6517a74e6b2681c
Sikander Nawaz <121254651+sikander-nawaz@users.noreply.github.com>
2025-01-29 21:35:46 +0500

    Time: 0 ms (100.00%) | Memory: 52.1 MB (28.98%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  let runningSum = 0;
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    runningSum += nums[i];
+    result.push(runningSum);
+  }
+  return result;
+};
