eee2c6a74e83c4ebaec4ea4ef930e01351a0b14d
Sikander Nawaz <121254651+sikander-nawaz@users.noreply.github.com>
2025-01-29 21:50:20 +0500

    Time: 2 ms (17.08%) | Memory: 50.7 MB (38.37%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  let result = [];
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= Math.max(...candies)) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+  return result;
+};
