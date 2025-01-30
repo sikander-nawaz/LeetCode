090ece39512b7980a6745f0476b3550cdd1190de
Sikander Nawaz <121254651+sikander-nawaz@users.noreply.github.com>
2025-01-30 22:46:09 +0500

    Time: 1 ms (39.04%) | Memory: 50.7 MB (46.60%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let str = n.toString(2);
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "1") {
+      count++;
+    }
+  }
+
+  return count;
+};
